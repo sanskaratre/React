a7689395cc8a2f9a8612aaa94a5308ecf297f12a
atresanskar <atresanskar55@gmail.com>
2022-03-28 14:13:10 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ExpenceItem from "./components/ExpenceItem";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenceItem>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+      </ExpenceItem>
     </div>
   );
 }
---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,12 @@
+function ExpenceItem (){
+    return <div>
+        <h2>Expence item!</h2>
+    <div>
+    <p>Food Rs 10</p>
+    <p>Petrol Rs 100</p>
+    <p>Movies Rs 200</p>
+    </div>
+        </div>
+   
+}
+export default ExpenceItem;
