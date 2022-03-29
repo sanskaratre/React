7d36117ef5c7ebec40ed94b6e4c62876822fdad0
atresanskar <atresanskar55@gmail.com>
2022-03-29 09:56:08 +0530

    passing_data_using_map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import ExpenseItem from "./components/ExpenceItem";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      LocationOfExpenditure: 'bhopal',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2',
+      LocationOfExpenditure: 'jaipur',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      LocationOfExpenditure: 'indore',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      LocationOfExpenditure: 'delhi',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+     
+      {
+        expenses.map((expens) => {
+          return (
+
+            <ExpenseItem
+            id = {expens.id}
+            title = {expens.title}
+            LocationOfExpenditure = {expens.LocationOfExpenditure}
+            amount = {expens.amount}
+            date = {expens.date}>        
+          </ExpenseItem>
+          )
+       })
+      } 
     </div>
+  
   );
 }
 
